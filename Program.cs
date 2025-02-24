a3c81c8798172fd7e458ed36b6458f96dc284048
Ecrin Nazar Aybay <118214765+ecrinaybay@users.noreply.github.com>
2025-02-24 23:03:43 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+class Car
+{
+    public string Name { get; set; }
+    public virtual void ShowTransmission()
+    {
+        Console.WriteLine($"{Name} : Vites bilgisi belirtilmemiş.");
+    }
+}
+
+class BMW : Car
+{
+    public BMW() { Name = "BMW"; }
+    public override void ShowTransmission()
+    {
+        Console.WriteLine($"{Name} : Düz viteslidir.");
+    }
+}
+
+class Porsche : Car
+{
+    public Porsche() { Name = "Porsche"; }
+    public override void ShowTransmission()
+    {
+        Console.WriteLine($"{Name} : Otomatik viteslidir.");
+    }
+}
+
+// Diğer araçlar:
+class Mercedes : Car
+{
+    public Mercedes() { Name = "Mercedes"; }
+    public override void ShowTransmission()
+    {
+        Console.WriteLine($"{Name} : Otomatik viteslidir.");
+    }
+}
+
+class Togg : Car
+{
+    public Togg() { Name = "Togg"; }
+    public override void ShowTransmission()
+    {
+        Console.WriteLine($"{Name} : Elektriklidir, tek viteslidir.");
+    }
+}
+
+class Audi : Car
+{
+    public Audi() { Name = "Audi"; }
+    public override void ShowTransmission()
+    {
+        Console.WriteLine($"{Name} : Otomatik viteslidir.");
+    }
+}
+
+class Toyota : Car
+{
+    public Toyota() { Name = "Toyota"; }
+    public override void ShowTransmission()
+    {
+        Console.WriteLine($"{Name} : Hem otomatik hem manuel vites seçenekleri vardır.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Car[] cars = { new BMW(), new Porsche(), new Mercedes(), new Togg(), new Audi(), new Toyota() };
+
+        foreach (var car in cars)
+        {
+            car.ShowTransmission();
+        }
+    }
+}
